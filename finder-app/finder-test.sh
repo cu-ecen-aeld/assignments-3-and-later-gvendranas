3fe06754101b0b77b2cffeb52aa2513682dc0679
gvendranas <gilles.vendranas@gmail.com>
2024-04-15 16:07:03 +0200

    Modified finder-app

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -34,7 +34,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+assignment=`cat ${NEW_DIR}/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -56,7 +56,7 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
