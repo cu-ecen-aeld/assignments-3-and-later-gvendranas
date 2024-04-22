e3d1238b9bd2941a74d0a98049e56519458165a5
gvendranas <gilles.vendranas@gmail.com>
2024-04-23 00:41:51 +0200

    Another try

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,18 +1,18 @@
 #! /bin/sh
 
 case "$1" in
-  start)
-    echo "Starting aesdsocket"
-    start-stop-daemon -S aesdsocket -- -d
-    ;;
-  stop)
-    echo "Stopping aesdsocket"
-    start-stop-daemon -K aesdsocket
-    ;;
-  *)
-    echo "Usage: aesdsocket-start-stop.sh {start|stop}"
+    start)
+        echo "Starting aesdsocket"
+        /usr/bin/aesdsocket -d
+        echo "Done"
+        ;;
+    stop)
+        echo "Stopping aesdsocket"
+        killall aesdsocket
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
     exit 1
-    ;;
 esac
 
 exit 0
