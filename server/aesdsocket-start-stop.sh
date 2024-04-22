d460fd71efede13565acc9f2dda259fe35db0b61
gvendranas <gilles.vendranas@gmail.com>
2024-04-22 19:48:25 +0200

    [Assignment 5 Part 2] Adding daemon start and stop script

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+case "$1" in
+    start)
+        echo "Starting server"
+        start-stop-daemon -S --oknodo -n aesdsock -a /usr/bin/aesdsocket -- -d --verbose
+        ;;
+    stop)
+        echo "Terminating server"
+        start-stop-daemon -K --oknodo -n aesdsocket --verbose
+        ;;
+    test)
+        echo "Testing server"
+        start-stop-daemon --test -S --oknodo -n aesdsocket -a /usr/bin/aesdsocket --verbose
+        ;;
+    *)
+        echo "Invalid Option!"
+        exit 1
+        ;;
+esac
+
+exit 0
